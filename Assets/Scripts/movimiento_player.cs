4707d60a791343d4190fb29dedf56da7e9ca9c81
xjavierf <javier_fr_7@hotmail.com>
2020-12-03 13:48:47 +0100

    armas attached a jugador

---
+++ Assets/Scripts/movimiento_player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento_player : MonoBehaviour
+{
+    public float velocidad;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
